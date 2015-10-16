7c632ad6fd1a06a2896a897638b602d6fd2cbdcb
onekorg <tonydelun@gmail.com>
2015-10-15 21:01:13 -0700

    added academic history

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ var auth = require('./auth');
 var grades = require('./grades');
 var information = require("./information");
 var schedule = require('./schedule');
+var academicHistory = require('./academicHistory');
 var setRequestEndpoint = require("../helpers/setRequestEndpoint")
 
 router.post('/login', auth.login);
 router.get('/api/schedule',setRequestEndpoint(schedule));
 router.get('/api/grades',setRequestEndpoint(grades));
 router.get('/api/general',setRequestEndpoint(information));
+router.get('/api/academicHistory', setRequestEndpoint(academicHistory));
 
 module.exports = router;
